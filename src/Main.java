4bb0547ef2e5e8f333b6d71f7d2282ca98ee0edc
afzalmalik6432 <afzalmalik6432@gmail.com>
2023-01-29 14:28:29 +0530

    Adding project

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            test1();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void test1() throws IOException, InterruptedException {
+        String url = "https://jsonplaceholder.typicode.com/albums/";
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+    }
+}
